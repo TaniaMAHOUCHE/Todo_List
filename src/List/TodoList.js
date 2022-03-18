e8fe2bd002e07b41e35c1567ab6d9483c882f2d3
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2022-03-18 15:24:18 +0100

    Update Todo List

--- src/List/TodoList.js
+++ src/List/TodoList.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class TodoList extends Component {
     constructor() {
         super();
+
         this.state = {
             userInput: '',
-            items: []
+            items: [],
+            color: '',
+            tasksCount: 0
         };
     }
 
@@ -19,8 +22,9 @@ class TodoList extends Component {
         event.preventDefault();
         this.setState({
             userInput: '',
-            items: [...this.state.items, this.state.userInput]
-        });
+            items: [...this.state.items, this.state.userInput],
+            
+        });  
     }
 
     deleteTodo(item) {
@@ -32,11 +36,23 @@ class TodoList extends Component {
         });
     }
 
+    markDone(item) {
+        const statut = this.state.color.slice();
+
+        if (statut === ''){
+            this.setState({ color: "red" });
+        } else {
+            this.setState({color: ''});
+        }
+        console.log(statut)
+    }
+
     renderTodos() {
         return this.state.items.map((item) => {
             return (
                 <div key={item}>
-                    {item} <button onClick={this.deleteTodo.bind(this, item)}>Supprimer</button>
+                    <div style = {{color: this.state.color}}>{item}</div><button onClick={this.deleteTodo.bind(this, item)}>Supprimer</button>
+                    <button onClick={this.markDone.bind(this, item)}>✅</button>
                 </div>    
             );
         });
@@ -55,7 +71,7 @@ class TodoList extends Component {
                             onChange={this.onChange.bind(this)}
                         />
                         <div className="submit-button" >
-                            <button onClick={this.addTodo.bind(this)} >Ajouter</button>
+                            <button onClick={this.addTodo.bind(this)}>Ajouter</button>
                         </div>
                     </form>
                 </div>
